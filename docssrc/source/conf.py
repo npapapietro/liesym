1ea8fbca01b9c277c14492f1a97cd4d8d3075c4f
Nathan Papapietro <npapapietro95@gmail.com>
2021-02-25 20:24:57 -0600

    github issues

--- docssrc/source/conf.py
+++ docssrc/source/conf.py
@@ -31,7 +31,7 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc', 'sphinx_math_dollar', 'sphinx.ext.mathjax'
+    'sphinx.ext.autodoc', 'sphinx_math_dollar', 'sphinx.ext.mathjax', 'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
