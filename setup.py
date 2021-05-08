8a76f6dcf70fbd938c9250a9acd2f0c423dfa096
Nathan Papapietro <npapapietro95@gmail.com>
2021-05-08 17:00:29 -0500

    fix

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import find_packages, setup
 from setuptools_rust import RustExtension, Binding
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup_requires = ['setuptools-rust>=0.11.6', 'pytest-runner']
 install_requires = [
@@ -32,5 +37,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-    ]
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
