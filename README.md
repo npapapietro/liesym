c093d32ceb59f2456410416acf28140de1a67c3d
Nathan Papapietro <npapapietro95@gmail.com>
2021-05-09 00:35:33 -0500

    Packaging test (#8)

--- README.md
+++ README.md
@@ -35,8 +35,6 @@ imported in `./liesym/__init__.py`
 ├── README.md           # you are here
 ├── liesym              # python module
 ├── notebooks           # jupyter notebooks
-├── setup.cfg           # pytest config
-├── setup.py            # python setuptools file
 ├── src                 # rust source code
 ├── tests               # python tests
 └── ...                 # other stuff
--- build-wheels.sh
+++ build-wheels.sh
@@ -6,11 +6,17 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 cd /io
 
-for PYBIN in /opt/python/cp{35,36,37,38,39}*/bin; do
+for PYBIN in /opt/python/cp{37,38,39}*/bin; do
     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
     "${PYBIN}/python" setup.py bdist_wheel
 done
 
+"${PYBIN}/python" setup.py sdist
+
 for whl in dist/*.whl; do
     auditwheel repair "$whl" -w dist/
+done
+
+for whl in dist/*-linux*; do
+    rm "$whl"
 done
