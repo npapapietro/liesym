8dbbff8505b419256df66344dc11cfccb79fd08d
Nathan Papapietro <npapapietro95@gmail.com>
2021-07-11 15:26:40 -0500

    Fixing ver 0.4.1

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.8
 
 RUN pip install -U pip
-RUN pip install liesym
+COPY . .
+RUN python setup.py bdist_wheel
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 
 setup(
     name='liesym',
-    version='0.3.0',
+    version='0.4.1',
     description='Sympy Lie Algebra extensions powered by rust.',
     rust_extensions=[RustExtension(
         'liesym.liesym',
