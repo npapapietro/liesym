9dc8ad652af70ba8b1154fc545d73494a75c8441
Nathan Papapietro <npapapietro95@gmail.com>
2021-07-20 16:02:15 -0500

    Release0.5.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 
 setup(
     name='liesym',
-    version='0.5.1',
+    version='0.5.2',
     description='Sympy Lie Algebra extensions powered by rust.',
     rust_extensions=[RustExtension(
         'liesym.liesym',
