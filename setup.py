7b0839dd3a9e47eedc38ef82080233ba86d16347
Nathan Papapietro <npapapietro95@gmail.com>
2021-08-24 14:11:13 -0500

    Bump bad publish again

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='liesym',
-    version='0.5.4',
+    version='0.5.5',
     description='Sympy Lie Algebra extensions powered by rust.',
     rust_extensions=[RustExtension(
         'liesym.liesym',
